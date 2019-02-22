aa13a84286e2538624c60a616199012ad8418a4e
Ruben <rs@artworks.io>
2019-02-22 15:55:29 +0100

    Updates

---
+++ precache-manifest.ee7a3eeadfae0f77fab90d6678fe971e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c2090dac7c7558f6711",
+    "url": "/static/css/main.c4ec11df.chunk.css"
+  },
+  {
+    "revision": "7c2090dac7c7558f6711",
+    "url": "/static/js/main.7c2090da.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "608742d1cac983c1ce7e",
+    "url": "/static/js/2.608742d1.chunk.js"
+  },
+  {
+    "revision": "df891cff32326ad17617273c491d9172",
+    "url": "/index.html"
+  }
+];
