87c87f4fa505effccb3ddc25536f571dd97cc188
Ruben <rs@artworks.io>
2021-11-08 22:09:54 +0100

    Musche

---
+++ precache-manifest.f4a3fd3e85ca9c55868997648ddd59a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8658728b68cf471b258",
+    "url": "/electric/static/css/main.830891c6.chunk.css"
+  },
+  {
+    "revision": "a8658728b68cf471b258",
+    "url": "/electric/static/js/main.a8658728.chunk.js"
+  },
+  {
+    "revision": "3f608a0f182832fe3dbd",
+    "url": "/electric/static/js/runtime~main.3f608a0f.js"
+  },
+  {
+    "revision": "afff0dd2e64cbb71c0d2",
+    "url": "/electric/static/js/2.afff0dd2.chunk.js"
+  },
+  {
+    "revision": "18eca67831b929dba105f041ee8c18cf",
+    "url": "/electric/static/media/mustasch.18eca678.jpeg"
+  },
+  {
+    "revision": "8a8166da6b85189a3b198de3fadef506",
+    "url": "/electric/index.html"
+  }
+];
