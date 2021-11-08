807a5e9e8e3114a2ad66838ecc7ffdbc630e7dec
Ruben <rs@artworks.io>
2021-11-08 22:15:22 +0100

    Updates

---
+++ precache-manifest.f628c1dd8ac40bbbd499f3ccda988578.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8658728b68cf471b258",
+    "url": "/static/css/main.830891c6.chunk.css"
+  },
+  {
+    "revision": "a8658728b68cf471b258",
+    "url": "/static/js/main.a8658728.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "afff0dd2e64cbb71c0d2",
+    "url": "/static/js/2.afff0dd2.chunk.js"
+  },
+  {
+    "revision": "18eca67831b929dba105f041ee8c18cf",
+    "url": "/static/media/mustasch.18eca678.jpeg"
+  },
+  {
+    "revision": "66221dc37396721381ce5bfcae38e4e3",
+    "url": "/index.html"
+  }
+];
